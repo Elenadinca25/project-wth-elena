94030e18490d16cb98be1367cdddd78a8dd162c4
Elenadinca25 <elenadinca25@gmail.com>
2020-05-19 22:18:43 +0100

    owner class create not finished

---
+++ app/controllers/owners_controller.rb
@@ -0,0 +1,31 @@
+class OwnersController < ApplicationController
+
+    def index
+        @owners = Owner.all 
+    end
+
+    def show
+        @owner = Owner.find(params[:id])
+    end
+
+
+    def new
+        @owner = Owner.new
+    end
+
+    def create 
+        @owner = Owner.create(owner_params)
+        redirect_to @owner
+    end
+
+
+    def edit
+        @owner = Owner.find(params[:id])
+    end
+
+
+    private
+    def owner_params
+        params.require(:owner).permit(:name)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :pets, only: [:index, :show, :new, :create, :edit]
   resources :adoptions
+  resources :owners, only: [:index, :show, :new, :create, :edit]
 end
